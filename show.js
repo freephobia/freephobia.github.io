64138ec69ae88a7e991f9f1a268c38801944fdcd
hssrgov <88079797+hssrgov@users.noreply.github.com>
2025-07-25 20:25:50 +0800

    修改

--- show.js
+++ show.js
@@ -9,13 +9,13 @@ function getQ(hname)//获取参数
 	}
     return null;
 }
-const TITLE_TAIL=" - 哈儿实验室危险哈儿数据库 БДОХЛХ";
+const TITLE_TAIL=" - 哈儿实验室危险哈儿数据库 БДОХЛХ",HAER_GROUP_ENDING=["","团","簇"];//表示这是一个危险哈儿团/簇，使得ХИРГ名称由“XXX哈儿”变为“XXX哈儿团/簇”
 const dg=["类人型哈儿","高智商哈儿","中智商哈儿","低智商哈儿","化学物质型哈儿","待销毁哈儿","已销毁哈儿","外逃哈儿","不存在此哈儿","已灭亡哈儿"];//各类哈儿综合能力等级（ХИРГ）的名称
 //           0             1            2       3           4            5         6           7         8           9
 const dgclr=["#00FF00","#AA00FF","#FFFF00","#00FFFF","#CC0000","#D06A0D","#6B6895","#2A5750","#FFFFFF","#88AAEE"];//每个ХИРГ的对应颜色
 const clrs=["#FFAAAA","#FFFFAA","#AAFFAA","#AAFFFF","#FFAAFF","#AAFFFF"];//链接颜色，共6个；数据库存储文件URL
 const ne_title="不支持"+TITLE_TAIL,ne_body="<h1 style=\" color: #FFAAAA;\">浏览器版本太旧，请使用更高版本的浏览器</h1>",XMLFILEURL_ORI="haerlib.xml";//База Данных Опасных Хаеры Лаборатории Хаера=БДОХЛХ
-const list1=["[Uns]","[br]","[lstr]","[lstrw]","[rstr]","[flagoj]","[la]","[las]","[lc]","[cfls]","[sub]","[rsub]","[sup]","[rsup]"],list2=["<strong style=\"color: #FF0000;\">[禁止显示]</strong>","\n<br>\n","<strong style=\"color: #FF0000;\">","<strong>","</strong>","\n<br>\n<strong style=\"color: #CC0000;\">（注：图片是此哈儿的个人旗帜，由此哈儿亲自设计）</strong>","<a target=\"_blank\" href=\"index.html?haer=","<a target=\"_blank\" href=\"","</a>","ШИЯЧ","<sub>","</sub>","<sup>","</sup>"];//替换的标识符，应尽快修改
+const list1=["[Uns]","[br]","[lstr]","[lstrw]","[rstr]","[flagoj]","[la]","[las]","[lc]","[cfls]","[sub]","[rsub]","[sup]","[rsup]"],list2=["<strong style=\"color: #FF0000;\">[禁止显示]</strong>","\n<br>\n","<strong style=\"color: #FF0000;\">","<strong>","</strong>","\n<br>\n<strong style=\"color: #CC0000;\">（注：图片是此哈儿的个人旗帜，由此哈儿亲自设计）</strong>","<a target=\"_blank\" href=\"/?haer=","<a target=\"_blank\" href=\"","</a>","ШИЯЧ","<sub>","</sub>","<sup>","</sup>"];//替换的标识符，应尽快修改
 var hname,born,des,dgs,info,haerona,dgmhx,age,die,xmlfileurl,localurl=window.location.protocol+"//"+window.location.host;//hname：名称（临时存储用的变量），born：出生年份，des：概要，dgs：ХИРГ（临时存储用的变量），info：信息（描述），haerona：此哈儿的负责人，dgmhx：哈儿性质（0=哈儿，1=哈儿团，2=哈儿簇，临时存储用的变量）,age：年龄（临时存储用的变量），die：被销毁或死亡年份（临时存储用的变量，用以标注年龄），xmlfileurl：XML文件路径，localurl：当前主域名
 var mhn,minhn,thisyear,actualminhn,rg,qj;//mhn为最大编号，minhn为最小编号，直接影响到索引的表格行数和“当前可用编号”的数值；thisyear为当前年份（公历），用于计算哈儿的年龄；actualminhn为实际最小哈儿编号；rg为循环替换如[br]之类的简略符号的循环次数，32次足够多；可用哈儿区间
 var xmlDoc,date=new Date();
@@ -30,11 +30,18 @@ function jump2notice()
 	window.location.href="notice.html";
 	return;
 }
-
-function about()//输出版权信息
+function about(division_num,_rus)//输出版权信息
 {
-  document.getElementById("about_index").innerHTML="哈儿实验室 &copy; 2018-"+thisyear+" 保留所有权利。<br>Лаборатория Хаера &copy; 2018-"+thisyear+" Все права защищены.";
-  return;
+    var _name="哈儿实验室",_name_rus="Лаборатория Хаера",tmp=String(division_num);
+    if(division_num>=0)
+    {
+        if(division_num<10)tmp="0"+String(division_num);
+        _name+=tmp+"分部";
+        _name_rus="Подразделение "+tmp+" Лаборатории Хаера";
+    }
+    if(_rus)document.getElementById("_copyright").innerHTML=_name+" &copy; 2018-"+thisyear+" 保留所有权利。<br>"+_name_rus+" &copy; 2018-"+thisyear+" Все права защищены.";
+    else document.getElementById("_copyright").innerHTML=_name+" &copy; 2018-"+thisyear+" 保留所有权利。";
+    return;
 }
 function tproc()//预处理（XMLHttpRequest方法）
 {
@@ -114,7 +121,7 @@ function run()//БДОХЛХ主页面运行函数
 		// info=info.replace("[lstrw]","<strong>");//[lstrw]=加粗字体开始
 		// info=info.replace("[rstr]","</strong>");//[rstr]=（红色）加粗字体结束
 		// info=info.replace("[flagoj]","\n<br>\n<strong style=\"color: #CC0000;\">（注：图片是此哈儿的个人旗帜，由此哈儿亲自设计）</strong>");//哈儿旗帜标识
-		// info=info.replace("[la]","<a target=\"_blank\" href=\"index.html?haer=");
+		// info=info.replace("[la]","<a target=\"_blank\" href=\"/?haer=");
 		// info=info.replace("[las]","<a target=\"_blank\" href=\"");
 		for (let i=0;i<list1.length;++i)info=info.replace(list1[i],list2[i]);
 		info=info.replace("[lb]","\" style=\"color: "+clrs[Math.floor(Math.random()*60)%6]+";\">");
@@ -133,22 +140,11 @@ function run()//БДОХЛХ主页面运行函数
 	document.getElementById("info").innerHTML=info;
 	document.getElementById("img").src="haer-img\\"+idd+".jpg";
 	document.getElementById("img").title=hname+" - "+idd;//这几行内容将大部分要展示的页面内容输出到页面，包括：页面标题、哈儿名称、概要、哈儿编号、哈儿年龄、信息（描述）、哈儿（团）的图片或关于此哈儿（团）的图片
-	if(dgmhx==0)//输出ХИРГ
-	{
-		document.getElementById("dg").innerHTML=dg[dgs];
-	}
-	else if(dgmhx==1)
-	{
-		document.getElementById("dg").innerHTML=dg[dgs]+"团";//表示这是一个危险哈儿团，使得ХИРГ名称由“XXX哈儿”变为“XXX哈儿团”
-	}
-	else if(dgmhx==2)
-	{
-		document.getElementById("dg").innerHTML=dg[dgs]+"簇";//表示这是一个危险哈儿簇，使得ХИРГ名称由“XXX哈儿”变为“XXX哈儿簇”
-	}
+	document.getElementById("dg").innerHTML=dg[dgs]+HAER_GROUP_ENDING[dgmhx];//输出ХИРГ
 	document.getElementById("dg").style="color: "+dgclr[dgs]+";";//将颜色设置为ХИРГ对应的颜色
 	document.getElementById("haerona").innerHTML=haerona;//输出哈儿的负责人
 	document.getElementById("haernum").innerHTML=qj;//输出当前可用哈儿（这两个变量直接关系到索引的运行！！）
-  	about();//输出版权信息
+  	about(-1,true);//输出版权信息
 	return;
 }
 function runtable()//БДОХЛХ索引运行函数
@@ -169,43 +165,16 @@ function runtable()//БДОХЛХ索引运行函数
 		dgsv=dg[dgsest];//获取此哈儿对应的ХИРГ名称
 		if(dgmht==1)dgsv+="团";//如果是哈儿团就使ХИРГ名称由“XXX哈儿”变为“XXX哈儿团”
 		else if(dgmht==2)dgsv+="簇";
-		rng+="<tr><td style=\"color: #AAFFAA;\">"+j+"</td><td><a style=\"color: #AAFFFF;\" href=\"index.html?haer="+j+"\" target=\"_blank\">"+hnlt+"</a></td><td style=\"color: #FFFFAA;\">"+dgsv+"</td></tr>\n";//添加每行的内容
+		rng+="<tr><td style=\"color: #AAFFAA;\">"+j+"</td><td><a style=\"color: #AAFFFF;\" href=\"/?haer="+j+"\" target=\"_blank\">"+hnlt+"</a></td><td style=\"color: #FFFFAA;\">"+dgsv+"</td></tr>\n";//添加每行的内容
 	}
 	document.getElementById("table").innerHTML=rng;//输出到页面
-  	about();//输出版权信息
+  	about(-1,true);//输出版权信息
 	return;
 }
 function nextbutton(ifnext)//БДОХЛХ主页面翻页按钮运行函数，若ifnext=1则翻到后一个哈儿，否则翻到前一个哈儿
 {
 	let idd=getQ("haer");//idd：请求的哈儿编号
-	if(ifnext==1)window.location.href="index.html?haer="+(Number(idd)+Number(1));//向后翻页
-	else window.location.href="index.html?haer="+(Number(idd)-Number(1));//向前翻页
+	if(ifnext==1)window.location.href="?haer="+(Number(idd)+Number(1));//向后翻页
+	else window.location.href="?haer="+(Number(idd)-Number(1));//向前翻页
 	return;
 }
-function test001()//测试能否连接到hssrgov网站
-{
-	return "reply";
-}
-function test002(string)//测试能否连接到hssrgov网站并测试js是否正常运行
-{
-	return "reply: req="+string;
-}
-//function tmpshow1()//show.js 2022年初大改中帮助修改的临时函数
-//{
-//	var str="";
-//	for(var i=0;i<=mhn;++i)
-//	{
-//		str+="//"+i+"<br>";
-//		str+=hnl[i]+"<br>";
-//		str+="!出生年份"+"<br>";
-//		str+="!概要"+"<br>";
-//		str+=dgses[i]+"<br>";
-//		str+="!负责人"+"<br>";
-//		str+=dgmh[i]+"<br>";
-//		str+="!被销毁或死亡的年份"+"<br>";
-//		str+="!哈儿详细信息"+"<br>";
-//		str+="[info-end]<br><br>";
-//	}
-//	document.getElementById("show").innerHTML=str;
-//	return;
-//}
